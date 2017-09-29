30098222eb0e51bb9b6368ad1077c1b1288b642d
cindyliu-yb <cliu@yoobic.com>
2017-09-29 16:22:42 +0100

    WIP

--- src/components/form-input-base/form-input-base.tsx
+++ src/components/form-input-base/form-input-base.tsx
@@ -10,6 +10,12 @@ export class FormInputBase {
 
   @Prop() type: string = 'text';
 
+  @Prop() label: string;
+
+  @Prop() mode: string;
+
+  @Prop() theme: string = 'black';
+
   @Prop({ mutable: true }) value: string;
 
   @Event() valueChange: EventEmitter;
@@ -24,15 +30,44 @@ export class FormInputBase {
   }
 
   inputChanged(ev: any) {
+    // from view to the model
+    let val = ev.target && ev.target.value;
+    if (this.type === 'time') {
+        this.value = this.convertTimeToDate(val);
+    } else {
+        this.value = val;
+    }
     this.value = ev.target && ev.target.value;
+    // add a setter for the value
     this.valueChange.emit(this.value);
   }
 
+  convertTimeToDate(val) {
+    return val;
+  }
+  getColorClassList() {
+
+  }
+
   render() {
-    return (
-      <div>
-        <input value={this.value} onInput={this.inputChanged.bind(this)}></input>
-      </div>
-    );
+    if (this.mode === 'web') {
+      return (
+        <div class={this.theme}>
+          <label>{ this.label }:
+            <div>
+              <input value={this.value} onInput={this.inputChanged.bind(this)}></input>
+            </div>
+          </label>
+        </div>
+      )
+    } else {
+      return (
+        <div class={this.theme}>
+          <label>{ this.label }:
+              <input value={this.value} onInput={this.inputChanged.bind(this)}></input>
+          </label>
+        </div>
+      )
+    }
   }
 }
